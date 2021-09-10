7d979609f88d9d264535f1a18fba599c47b2c5c0
MD5-Hashm <47696465+MD5-Hashm@users.noreply.github.com>
2021-09-10 13:03:33 -0400

    Add files via upload

---
+++ LetsSpamDavidAdjustable.py
@@ -0,0 +1,21 @@
+import random
+import time
+from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Listener
+
+keyboard = Controller()
+
+message = input('Please input the message you would like to spam: ')
+delay = input('Please input the delay between each message: ')
+
+delayint = int(delay)
+
+hi = 1
+
+time.sleep(4)
+
+while hi == 1:
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(delayint)
---
+++ LetsSpamDavidDiscord.py
@@ -0,0 +1,47 @@
+import random
+import time
+from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Listener
+
+keyboard = Controller()
+
+message = input('Please input the message you would like to spam: ')
+
+hi = 1
+
+time.sleep(4)
+
+while hi == 1:
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.58)
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.58)
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.58)
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.58)
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.58)
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.58)
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.58)
+    keyboard.type(message)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.16)
+    time.sleep(3)
